c3ff76aa35ce1c68554c7df80eac8db6bb202cb8
Kevin Wander <kwander31@gmail.com>
2023-01-29 20:01:19 -0500

    Update

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+"""
+Name: Kevin Wander
+Email: kevin.wander30@myhunter.cuny.edu
+Resources:  My notes
+I attended lecture today.
+Row: Last Row
+Seat:  101
+"""
 
-print("hello world")
+#takes name of file as input, makes dictionary of the lines of the file.
+
+
+#name = input("Enter file name, with extension: ")
+
+def make_dict(filename, sep=': '):
+    dict = {}
+    with open(filename) as md:
+        counter = 0
+        for line in md:
+            dict[counter] = line
+            counter+=1
+    return dict
+
+print(filename())
+
